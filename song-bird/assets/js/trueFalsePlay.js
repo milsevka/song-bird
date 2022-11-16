0c0df5c7e4fb9423dfafb1cc1fe7aaf48a6d0335
Milsevka <lusindami99@gmail.com>
2022-11-16 15:47:31 +0300

    fix: change songs try false answer

--- song-bird/assets/js/trueFalsePlay.js
+++ song-bird/assets/js/trueFalsePlay.js
@@ -4,7 +4,7 @@ export function tryPlay() {
      let winone = localStorage.getItem("winNumb")
      if (winone == 1) {
         let audiotry = new Audio();
-        audiotry.src = "../../assets/songs/true.mp3";
+        audiotry.src = "../../assets/songs/zvuk-otvet-zaschitan-galochka.mp3";
         audiotry.play();
         audiotry.volume = 0.5;
      }
@@ -14,7 +14,7 @@ export function falsePlay() {
     let win = localStorage.getItem("winner")
     if (!win) {
         let audiofalse = new Audio();
-        audiofalse.src = "../../assets/songs/false.mp3";
+        audiofalse.src = "../../assets/songs/computer-keyboard-button-press-release_m1pp3tnd.mp3";
         audiofalse.play();
         audiofalse.volume = 0.5;
     }
